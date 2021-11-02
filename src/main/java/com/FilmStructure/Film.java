14c86bb220d577db1bb7c6f7656af34add8aeef3
abdul <abdul2213@gmail.comgit commitgit config --global user.email abdul2213@gmail.com>
2021-11-02 17:02:58 +0000

    first commit

--- src/main/java/com/FilmStructure/Film.java
+++ src/main/java/com/FilmStructure/Film.java
@@ -1,11 +1,19 @@
 package com.FilmStructure;
 
-    //Attributes
+import javax.persistence.*;
+
+//Attributes
+@Entity
 public class Film extends FilmCollection{
+    @Id
+    @GeneratedValue(strategy= GenerationType.TABLE)
+
+    private String filmID;
     private String title;
     private String genre;
     private Double duration;
     private Double rating;
+
 
     //Constructor
     public Film(String title, String genre, Double duration, Double rating){
@@ -13,6 +21,9 @@ public class Film extends FilmCollection{
         this.genre=genre;
         this.duration=duration;
         this.rating=rating;
+    }
+
+    public Film (){
     }
 
     //Method
@@ -23,6 +34,10 @@ public class Film extends FilmCollection{
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public String getFilmID() {return filmID;}
+
+    public void setFilmID(String filmID) {this.filmID = filmID;}
 
     public String getGenre() {
         return genre;
